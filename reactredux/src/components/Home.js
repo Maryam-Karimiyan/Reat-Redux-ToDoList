0f011b9927597cba1e7c4fc84fc60111eb32b20d
maryam <Ofogh.maryam14@gmail.com>
2022-07-26 12:02:34 +0430

    commiit

--- reactredux/src/components/Home.js
+++ reactredux/src/components/Home.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Button} from '@mui/material'
 import AddIcon from '@mui/icons-material/Add';
 import ToDos from './ToDos';
 function Home() {
+  
+  const [data,setData]=useState('');
+  console.log(data);
   return (
     <div className='container'>
         <section className='mt-3 text-center'>
             <h3>Enter Your Task</h3>
 
             <div className="todo col-lg-5 mx-auto d-flex justify-content-between">
-                <input name='task' className='form-control'/>
+                <input value={data} onChange={(e)=>setData(e.target.value)} name='task' className='form-control'/>
                 <Button variant='contained' style={{background:'#ee5253'}} className='mx-2'>
                    <AddIcon/>
                 </Button>
--- reactredux/src/components/ToDos.js
+++ reactredux/src/components/ToDos.js
@@ -2,23 +2,24 @@ import React from "react";
 import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
-import { lineHeight } from "@mui/system";
 
 function ToDos() {
   return (
     <div className="todo-data col-lg-5 mx-auto mt-2">
       <div
         className="todo-container d-flex column align-items-center justify-content-between px-2"
-        style={{ background: "#dcdde1", borderRadius: "3px", height: "45px" ,lineHeight:'-15px'}}
+        style={{
+          background: "#dcdde1",
+          borderRadius: "3px",
+          height: "45px",
+        }}
       >
-        <li style={{ listStyle: "none" }}>
-          reeee
-          <div className="edit-dit col-lg-3 py-2 d-flex justify-content-between align-items-center">
-            <ModeEditIcon style={{ color: "#3c40c6", cursor: "pointer" }} />
-            <DeleteIcon style={{ color: "red", cursor: "pointer" }} />
-            <RemoveRedEyeIcon style={{ color: "#1dd1a1", cursor: "pointer" }} />
-          </div>
-        </li>
+        <li style={{ listStyle: "none" }}>reeee </li>
+        <div className="edit-dit col-lg-3 py-2 d-flex justify-content-between align-items-center">
+          <ModeEditIcon style={{ color: "#3c40c6", cursor: "pointer" }} />
+          <DeleteIcon style={{ color: "red", cursor: "pointer" }} />
+          <RemoveRedEyeIcon style={{ color: "#1dd1a1", cursor: "pointer" }} />
+        </div>
       </div>
     </div>
   );
